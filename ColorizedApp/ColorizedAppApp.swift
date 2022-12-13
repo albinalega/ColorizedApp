52c268efe9e10f41acf35119785c50e164acd1e2
albinalega <lega.albina@mail.ru>
2022-12-13 17:19:31 +0900

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Альбина Лега on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
